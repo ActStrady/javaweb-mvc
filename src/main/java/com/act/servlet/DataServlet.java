be4d70bd91b3f7d30d54437c52d7b5aeafa3718e
王强 <ActStrady@tom.com>
2019-07-02 21:05:28 +0800

    菜单和退出功能

---
+++ src/main/java/com/act/servlet/DataServlet.java
@@ -0,0 +1,22 @@
+package com.act.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DataServlet", value = "/DataServlet")
+public class DataServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("jsp/data.jsp").forward(request, response);
+    }
+}
