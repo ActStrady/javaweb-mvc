eef67efa7beb6e76f89c4aa4fdb058ce179a8c70
王强 <ActStrady@tom.com>
2019-07-02 21:00:58 +0800

    完成对请求的拦截与处理，防止用户访问内部页面

--- src/main/java/com/act/filter/LoginFilter.java
+++ src/main/java/com/act/filter/LoginFilter.java
@@ -1,5 +1,7 @@
 package com.act.filter;
 
+import com.act.pojo.User;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -31,26 +33,27 @@ public class LoginFilter implements Filter {
 
         // 拦截请求的处理
         String uri = request.getRequestURI();
-        String user = (String) request.getSession().getAttribute("username");
-        System.out.println(uri);
+        User user = (User) request.getSession().getAttribute("user");
         // 拦截除了验证码和登录界面以外的请求
         if (!(uri.contains("/VerifyCodeServlet") || uri.contains("/LoginServlet"))) {
             // 不允许用户直接访问jsp
             if (uri.endsWith(".jsp")) {
+                // 用户没有登录就在登录页面显示无权限
                 if (user == null) {
                     request.setAttribute("error", "您无权访问该页面");
                     request.getRequestDispatcher("/LoginServlet").forward(request, response);
                     return;
-                }else {
-                    // TODO 对于已经登录的用户来说就应该跳动一个页面 暂时跳到其主页面
+                } else {
+                    // TODO 对于已经登录的用户来说就应该跳到一个页面 暂时跳到其主页面
                     request.getRequestDispatcher("main.jsp").forward(request, response);
                     return;
                 }
-
-            } else if (user != null) {
-                request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
-                return;
             }
+        }
+        // 用户session不失效的话就访问登录页面提交到主页面
+        if (uri.contains("/LoginServlet") && user != null) {
+            request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
+            return;
         }
         chain.doFilter(req, resp);
     }
